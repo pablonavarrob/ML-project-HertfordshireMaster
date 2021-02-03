8efb9142b6b1b463c377837426d2f09f8b314878
Pablo Navarro <55085296+pablonavarrob@users.noreply.github.com>
2021-02-03 17:01:07 +0100

    Update adql_rave_project.sql

--- adql_rave_project.sql
+++ adql_rave_project.sql
@@ -1,6 +1,5 @@
 SELECT
-t1.rave_obs_id , t1.spectrum_fits , t2.hrv_sparv , t2.hrv_error_sparv , t3. teff_cal_madera , t3.teff_error_madera , t3.logg_cal_madera , t3. logg_error_madera , t3.m_h_cal_madera , t3.m_h_error_madera , t3. snr_madera , t4.flag1_class , t4.flag2_class , t4.flag3_class , t4.
-w1_class , t4.w2_class , t4.w3_class
+t1.rave_obs_id , t1.spectrum_fits , t2.hrv_sparv , t2.hrv_error_sparv , t3.teff_cal_madera , t3.teff_error_madera , t3.logg_cal_madera , t3. logg_error_madera , t3.m_h_cal_madera , t3.m_h_error_madera , t3. snr_madera , t4.flag1_class , t4.flag2_class , t4.flag3_class , t4.w1_class , t4.w2_class , t4.w3_class
 FROM ravedr6.dr6_spectra AS t1
 JOIN ravedr6.dr6_sparv AS t2 ON t1.rave_obs_id = t2.rave_obs_id
 JOIN ravedr6.dr6_madera AS t3 ON t1.rave_obs_id = t3.rave_obs_id
